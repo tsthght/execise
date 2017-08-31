b54ee2b3775db3e704a49ec52f983a1d88d1f579
tsthght <781181214@qq.com>
2017-08-31 16:10:10 +0800

    modify config.pl

--- perl/web-perl/config.pl
+++ perl/web-perl/config.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-
+# Config::Tiny
 use Config::Tiny;
 my $config=Config::Tiny->new();
 $config=Config::Tiny->read('source.cnf');
@@ -9,3 +9,13 @@ my $admin_pwd = $config->{"mysql-proxy"}->{"admin-password"};
 $config->{"mysql-proxy"}->{"proxy-backend-addresses"} = "127.0.0.1:6000";
 $config->write("source.cnf");
 printf "$admin_user => $admin_pwd\n";
+
+# Config::Simple
+use Config::Simple;
+my $file=new Config::Simple;
+$file->read('source1.cnf');
+$cfg=$file->vars();
+my $d=$cfg->{"database.db"};
+my $u=$cfg->{"database.user"};
+my $p=$cfg->{"database.password"};
+print "$d::$u=>$p\n";
